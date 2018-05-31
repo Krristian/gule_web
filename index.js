ad9662e2e08fda0899ca12ad55b8b0d2f07a29ba
Krristian <36510549+Krristian@users.noreply.github.com>
2018-05-31 16:09:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,42 @@ firebase.auth().signInWithEmailAndPassword(userEmail, password).catch(function(e
 });
 }
 
+
+
+function obnovaHesla()
+{
+var auth = firebase.auth();
+var emailAddress = document.getElementById('email').value;
+
+auth.sendPasswordResetEmail(emailAddress).then(function() {
+  
+  window.alert("Email sent.");
+}).catch(function(error) {
+
+    var errorCode = error.code;
+    var errorMessage = error.message;
+     window.alert("Chyba: " + errorMessage);
+});
+
+
+}
+
+
+function registrace () 
+{
+var userEmail = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+
+firebase.auth().createUserWithEmailAndPassword(userEmail, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+   window.alert("Chyba: " + errorMessage);
+});
+  
+ 
+}
+
 function logout() 
 {
 
